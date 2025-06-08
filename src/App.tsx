5c5bbe83f63ac6593d96be5198e35e697a1f580e
Orange4Broom <orang4broom@gmail.com>
2025-06-08 17:34:33 +0200

    Přidání Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Icon } from "./components/elements/icon/Icon";
+
 export const App = () => {
   return (
-    <div>
-      <h1>Bruh</h1>
-    </div>
+    <section className="home-section" id="home">
+      <div className="home-section__content">
+        <p className="home-section__content__country">Based in czechia</p>
+        <h1 className="home-section__content__title">
+          Quality <span className="home-section__content__title--highlight">Design & Professionalism</span> in <span className="home-section__content__title--highlight">Web </span> Development
+        </h1>
+        <p className="home-section__content__description">
+          Hi, I’m Jakub, I create intuitive, visually stunning, and highly functional web applications.
+        </p>
+        <div className="home-section__content__buttons">
+          <button type="button" className="home-section__content__buttons__button--work">
+            See my work
+            <Icon name="angle-right" type="fas" />
+          </button>
+          <button type="button" className="home-section__content__buttons__button--cv">
+            <Icon name="file-arrow-down" type="fas" color="#4EB458" />
+            Download CV
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
